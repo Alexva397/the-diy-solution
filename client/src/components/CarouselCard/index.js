a836e078755589451c4bfa5c44aa169033fd3264
katelyn chadwell <kchadwell0226@gmail.com>
2021-07-08 17:43:50 -0500

    added some starter code to carousel component

---
+++ client/src/components/CarouselCard/index.js
@@ -0,0 +1,12 @@
+// Carousel component to be rendered later on Home page
+import React from "react";
+
+function CarouselCard() {
+    return (
+        // replace the below with your jsx (html/JavaScript hybrid)
+        <>
+        </>
+    )
+}
+
+export default CarouselCard;
