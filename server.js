a0d4886c0f643e5211f6e57e0bc62421b64b55bc
JP Eiler <jp.eiler97@gmail.com>
2021-07-07 21:26:21 -0500

    added to server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require('mongoose');
+const routes = require('./routes');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
