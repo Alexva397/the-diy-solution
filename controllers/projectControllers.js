ef11ab52964c4a26f766a53dc28523a722e1e02f
JP Eiler <jp.eiler97@gmail.com>
2021-07-11 14:59:03 -0500

    added types to project schema

--- controllers/projectControllers.js
+++ controllers/projectControllers.js
@@ -7,12 +7,24 @@ module.exports = {
           .then(dbModel => res.json(dbModel))
           .catch(err => res.status(422).json(err));
       },
+      findById: function(req, res) {
+        db.Project
+          .findById(req.params.id)
+          .then(dbModel => res.json(dbModel))
+          .catch(err => res.status(422).json(err));
+      },
       create: function(req, res) {
         db.Project
           .create(req.body)
           .then(dbModel => res.json(dbModel))
           .catch(err => res.status(422).json(err));
       },
+      update: function(req, res) {
+        db.Project
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+    },
       remove: function(req, res) {
         db.Project
           .findById({ _id: req.params.id })
--- models/project.js
+++ models/project.js
@@ -3,6 +3,12 @@ const Schema = mongoose.Schema;
 
 const projectSchema = new Schema({
     //Add document types here
+    title: {type: String, required: true },
+    description: String,
+    materials: Array,
+    photos: Array,
+    docs: Array,
+
 });
 
 const Project = mongoose.model('Project', projectSchema);
