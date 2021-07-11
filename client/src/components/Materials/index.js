0d9e6fa47b7611fb1c433ba919334f59669ce938
katelyn chadwell <kchadwell0226@gmail.com>
2021-07-11 18:17:30 -0500

    user input saving to state, API still WIP

--- client/src/components/Materials/index.js
+++ client/src/components/Materials/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import { Table, Thead, Tbody, Tr, Th, Td } from "react-super-responsive-table";
 import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css';
+import API from "../../utils/API";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -23,8 +24,51 @@ const useStyles = makeStyles((theme) => ({
     }
 }));
 
-function Materials() {
-    const classes = useStyles();
+function Materials(props) {
+    const classes = useStyles([]);
+    const [formObject, setFormObject] = useState({
+        id: 1,
+        item: "",
+        budgetPrice: "",
+        purchasePrice: "",
+    });
+
+    useEffect(() => {
+        loadItems()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    function loadItems(id) {
+        API.getProject(id)
+            .then(res => loadItems())
+            .catch(err => console.log(err));
+    };
+
+    function handleInputChange(event) {
+        const { name, value } = event.target;
+        setFormObject({ ...formObject, [name]: value })
+    };
+
+    function handleFormSubmit(event) {
+        event.preventDefault();
+        if (formObject.item && formObject.budgetPrice && formObject.purchasePrice) {
+            console.log(formObject);
+            API.saveProject({
+                id: 1,
+                item: formObject.item,
+                budgetPrice: formObject.budgetPrice,
+                purchasePrice: formObject.purchasePrice
+            })
+
+                .then(() => setFormObject({
+                    item: "",
+                    budgetPrice: "",
+                    purchasePrice: ""
+                }))
+                .then(() => loadItems())
+                .catch(err => console.log(err));
+        }
+    }
 
     return (
         <>
@@ -33,25 +77,37 @@ function Materials() {
                     <TextField
                         id="standard-textarea"
                         label="Item Name"
+                        name="item"
                         placeholder="Placeholder"
                         multiline
                         className={classes.textfield}
+                        onChange={handleInputChange}
+                        value={formObject.item}
+
                     />
                     <TextField
                         id="standard-textarea"
                         label="Budget Price"
+                        name="budgetPrice"
                         placeholder="Placeholder"
                         multiline
                         className={classes.textfield}
+                        onChange={handleInputChange}
+                        value={formObject.budgetPrice}
                     />
                     <TextField
                         id="standard-textarea"
                         label="Purchase Price"
+                        name="purchasePrice"
                         placeholder="Placeholder"
                         multiline
                         className={classes.textfield}
+                        onChange={handleInputChange}
+                        value={formObject.purchasePrice}
                     />
                     <Button
+                        disabled={!formObject.item && formObject.budgetPrice && formObject.purchasePrice}
+                        onClick={handleFormSubmit}
                         type="submit"
                         multiline
                         variant="outlined"
