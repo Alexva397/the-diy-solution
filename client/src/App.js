a2fc4b0c1d49776317a7e39cdcea031511cc2109
katelyn chadwell <kchadwell0226@gmail.com>
2021-07-13 08:42:39 -0500

    reworked router to pull up detail page for specific project IDs

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/SignIn";
 import Landing from "./pages/LandingPage";
-import Detail from "./pages/ProjectDetail";
+import Detail from "./pages/Detail";
 
 function App() {
   return (
@@ -13,13 +13,22 @@ function App() {
       <div className="App">
         <Router>
           <Nav />
-
           <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={Signup} />
-            <Route exact path="/landing" component={Landing} />
-            <Route exact path="/detail" component={Detail} />
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/login">
+              <Login />
+            </Route>
+            <Route exact path="/signup">
+              <Signup />
+            </Route>
+            <Route exact path="/landing">
+              <Landing />
+            </Route>
+            <Route exact path="/landing/:id">
+              <Detail />
+            </Route>
           </Switch>
         </Router>
       </div>
