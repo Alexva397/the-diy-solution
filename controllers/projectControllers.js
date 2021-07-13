b15b25e5ab548ffbfefde06de2a81f8f795e3d67
JP Eiler <jp.eiler97@gmail.com>
2021-07-13 11:39:16 -0500

    worked on landing page

--- controllers/projectControllers.js
+++ controllers/projectControllers.js
@@ -3,7 +3,7 @@ const db = require('../models');
 module.exports = {
     findAll: function(req, res) {
         db.Project
-          .find(req.query).sort({_id:-1})
+          .find(req.query).sort({_id:1})
           .then(dbModel => res.json(dbModel))
           .catch(err => res.status(422).json(err));
       },
