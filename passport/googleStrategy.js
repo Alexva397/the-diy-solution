9eec9a916b6b144d2e68c0e06042e256e4c5b014
Alexander Vadeboncoeur <asvadeboncoeur@gmail.com>
2021-07-13 20:40:44 -0500

    successful register router, creating new user. working on login passport auth, issue w/ routing merging for reference

--- passport/googleStrategy.js
+++ passport/googleStrategy.js
@@ -9,8 +9,13 @@ const googleStrategy = new GoogleStrategy(
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         callbackURL: 'http://localhost:3001/api/user/auth/google/callback',
     },
-    function(accessToken, refreshToken, profile, done) {
-        console.log(profile);
+    function (accessToken, refreshToken, profile, done) {
+        // let dispName = profile.dispalyName;
+        // let googleUsername = dispName.replace(/\s+/g, '');
+        console.log(profile)
+        const { id, dispalyName } = profile;
+
+        console.log()
         // User.findOrCreate({ googleId: profile.id }, function(err, user) {
         //     return done(err, user);
         // })
