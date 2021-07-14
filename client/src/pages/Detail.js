1fc111b89fef5051c3d30f5c0d02ff419eda9ce5
JP Eiler <jp.eiler97@gmail.com>
2021-07-14 13:23:47 -0500

    fixing merge

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from "react-router-dom";
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
@@ -70,21 +69,19 @@ function Detail() {
 
     const [project, setProject] = useState([])
 
-    const { id } = useParams()
     useEffect(() => {
-        API.getProject(id)
+        getProject()
+    }, [])
+
+    function getProject(_id) {
+        API.getProject(_id)
             .then(res => {
-                setProject(res.data)
-                console.log(res)
+
+                console.log(res);
             })
             .catch(err => console.log(err));
-    }, [id])
-
-    console.log("project", project);
-
-    console.log("title", project.title);
-    console.log("materials", project.materials)
-
+    }
+    
     const handleChange = (event, newValue) => {
         setValue(newValue);
     };
@@ -109,13 +106,10 @@ function Detail() {
                 </Tabs>
             </AppBar>
             <TabPanel value={value} index={0}>
-                <Summary title={project.title} />
+                <Summary />
             </TabPanel>
             <TabPanel value={value} index={1}>
-
-                <Materials key={project.title} item={project.item} budgetPrice={project.budgetPrice} purchasePrice={project.purchasePrice} />
-
-
+                <Materials />
             </TabPanel>
             <TabPanel value={value} index={2}>
                 <Photos />
