903a67c50ce5e868a259cb32df665dc108de64a2
ktp1451 <76793752+ktp1451@users.noreply.github.com>
2021-07-17 17:21:31 -0500

    updated styling

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -1,72 +1,26 @@
-import React from "react";
-// import Button from '@material-ui/core/Button';
-// import Card from '@material-ui/core/Card';
-// import CardActions from '@material-ui/core/CardActions';
-// import CardContent from '@material-ui/core/CardContent';
-// import CardMedia from '@material-ui/core/CardMedia';
-// import Grid from '@material-ui/core/Grid';
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
-// import Link from '@material-ui/core/Link';
-import Divider from "@material-ui/core/Divider";
+import React from 'react';
+import './hero.css';
+import '../../../src/App.css';
+// import video from '../../assets/videos/Nick_Offerman_TOH.mp4';
+// import { Player } from 'video-react';
 
-const useStyles = makeStyles((theme) => ({
-  icon: {
-    marginRight: theme.spacing(2),
-  },
-  heroContent: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(12, 0, 6),
-  },
-  cardGrid: {
-    paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8),
-  },
-  card: {
-    height: "100%",
-    display: "flex",
-    flexDirection: "column",
-  },
-  cardMedia: {
-    paddingTop: "56.25%", // 16:9
-  },
-  cardContent: {
-    flexGrow: 1,
-  },
-  footer: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(6),
-  },
-}));
-
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-export default function Album() {
-  const classes = useStyles();
-
+function Hero() {
   return (
-    <div className={classes.heroContent}>
-      <Container maxWidth="md">
-        <Typography
-          component="h1"
-          variant="h2"
-          align="center"
-          color="textPrimary"
-          gutterBottom
-        >
-          Your All-in-One Home Project Management Solution
-        </Typography>
-        <Divider variant="middle" />
-        <Typography variant="h4" align="center" color="textSecondary" paragraph>
-          Save Money. Improve Value. Get Organized.
-        </Typography>
-        <Typography variant="h5" align="center" color="textSecondary" paragraph>
-          Easy to use smart applications, content and data to manage your home
+    <div className='hero-container'>
+      <video src='../../assets/videos/video-1.mp4' autoPlay loop muted/>
+      {/* <video width="1500" controls>
+        <source src='../../assets/videos/video-1.mp4' type="video/mp4"></source>
+      </video> */}
+      {/* <Player>
+      <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" />
+    </Player> */}
+      <h1>Your All-in-One Home Project Management Solution</h1>
+      <h3>Save Money. Improve Value. Get Organized.</h3>
+      <p>Easy to use smart applications, content and data to manage your home
           for insurance, maintenance, remodeling, and financial purposes. It is
-          time to manage your largest asset!
-        </Typography>
-      </Container>
+          time to manage your largest asset!</p>
     </div>
   );
 }
+
+export default Hero;
--- client/src/components/Summary/index.js
+++ client/src/components/Summary/index.js
@@ -9,38 +9,43 @@ import Grid from "@material-ui/core/Grid";
 import CssBaseline from "@material-ui/core/CssBaseline";
 // Import Media
 // =========================================================  
+import "../Summary/Summary.css";
 // import top_hero from "../../assets/images/top_hero";
 // ========================================================= "
 
 const useStyles = makeStyles({
     title: {
         fontSize: 20,
+        color: '#173e43'
     },
-    // background: url('')
+    name: {
+        fontSize: 35,
+        
+    }
 });
 
 function Summary({ title }) {
     const classes = useStyles();
 
     return (
-        <Grid container>
-            <CssBaseline />
-            <Grid item xs={12}>
-                <Typography className={classes.title}>
-                    {title}
-                </Typography>
-                <Typography className={classes.title}>
-                    Project total: $2800
-                </Typography>
+            <Grid container>
+                <CssBaseline />
+                <Grid item xs={12}>
+                    <Typography className={classes.name}>
+                        {title}
+                    </Typography>
+                    <Typography className={classes.title}>
+                        Project total: $2800
+                    </Typography>
 
-                <Typography className={classes.title}>
-                    Cost to Date: $1200
-                </Typography>
-                <Typography className={classes.title}>
-                    Balance: $500
-                </Typography>
+                    <Typography className={classes.title}>
+                        Cost to Date: $1200
+                    </Typography>
+                    <Typography className={classes.title}>
+                        Balance: $500
+                    </Typography>
+                </Grid>              
             </Grid>
-        </Grid>
     );
 }
 
