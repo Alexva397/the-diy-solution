7b8433304137842e5179b1d115b5b45c134f9dde
Alexander Vadeboncoeur <asvadeboncoeur@gmail.com>
2021-07-19 19:01:33 -0500

    updated context, movid context provider to index.js so it is usable in app.js. brought in context in landing page

--- client/src/Context.js
+++ client/src/Context.js
@@ -3,18 +3,16 @@ import axios from "axios";
 
 export const userContext = createContext();
 
-export default function Context ({ children }) {
+export default function Context({ children }) {
 
     const [userObject, setUserObject] = useState(null);
     const [isAuthenticated, setIsAuthenticated] = useState(false);
-    
 
     useEffect(() => {
-        
+
         axios.get("/api/user/getuser", { withCredentials: true })
             .then((res) => {
                 if (res.data) {
-                    console.log(res.data.user);
                     setUserObject(res.data.user);
                     setIsAuthenticated(res.data.isAuthenticated);
                 }
