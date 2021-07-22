8ba836f39d2918f1b37f2df11675de99b29b2716
JP Eiler <jp.eiler97@gmail.com>
2021-07-21 22:20:41 -0500

    made summary page pretty

--- client/src/components/Summary/index.js
+++ client/src/components/Summary/index.js
@@ -6,6 +6,7 @@ import React from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Grid from "@material-ui/core/Grid";
+import Paper from '@material-ui/core/Paper'
 import CssBaseline from "@material-ui/core/CssBaseline";
 // Import Media
 // =========================================================  
@@ -15,16 +16,62 @@ import "../Summary/Summary.css";
 
 const useStyles = makeStyles({
     title: {
-        fontSize: 20,
-        color: '#173e43'
+        float: 'left',
+        fontSize: '20px',
+        marginLeft: '40px',
+        color: 'white'
     },
     name: {
-        fontSize: 35,
+        fontSize: '40px',
     },
     balance: {
-        fontSize: 20,
+        fontSize: '20px',
         color: 'rgb(255, 6, 6)',
     },
+    firstPaper: {
+        paddingTop: '50px',
+        paddingBottom: '50px',
+        borderBottomLeftRadius: '0px',
+        borderBottomRightRadius: '0px',
+        backgroundColor: '#dddfd4',
+        boxShadow: '0px 2px 2px #818181',
+        position: 'relative',
+        zIndex: '4'
+    },
+    secondPaper: {
+        paddingTop: '15px',
+        paddingBottom: '15px',
+        borderRadius: '0px',
+        backgroundColor: '#3fb0ac',
+        height: '75px',
+        position: 'relative',
+        zIndex: '3'
+
+    },
+    thirdPaper: {
+        paddingTop: '15px',
+        paddingBottom: '15px',
+        borderRadius: '0px',
+        backgroundColor: '#173e43',
+        height: '75px',
+        position: 'relative',
+        zIndex: '2'
+    },
+    lastPaper: {
+        paddingTop: '15px',
+        paddingBottom: '15px',
+        borderTopLeftRadius: '0px',
+        borderTopRightRadius: '0px',
+        backgroundColor: '#f7b733',
+        height: '75px',
+        boxShadow: '0px 2px 2px #818181',
+        position: 'relative',
+        zIndex: '1'
+    },
+    budget: {
+        marginLeft: 'auto',
+        marginRight: 'auto'
+    }
 });
 
 function Summary({ title, budget, totalSpent }) {
@@ -32,23 +79,44 @@ function Summary({ title, budget, totalSpent }) {
     const balance = budget - totalSpent;
 
     return (
-        <Grid container>
+        <div>
+        <h1>Your project finances, at a glance.</h1>
+        <Grid container direction= "column" className={classes.budget} xs={4}>
             <CssBaseline />
-            <Grid item xs={12}>
-                <Typography className={classes.name}>
-                    {title}
-                </Typography>
-                <Typography className={classes.title}>
-                    Budget: ${budget}
-                </Typography>
-                <Typography className={classes.title}>
-                    Cost to Date: ${totalSpent}
-                </Typography>
-                <Typography className={balance < 0 ? classes.balance : classes.title}>
-                    Balance: ${balance}
-                </Typography>
-            </Grid>
+           
+
+                    <Grid item>
+                    <Paper className={classes.firstPaper}>
+                            <Typography className={classes.name}>
+                                {title}
+                            </Typography>
+                        </Paper>
+                    </Grid>
+                    <Grid item>
+                    <Paper className={classes.secondPaper}>
+                            <Typography className={classes.title}>
+                                Budget: ${budget}
+                            </Typography>
+                        </Paper>
+                    </Grid>
+                    <Grid item>
+                    <Paper className={classes.thirdPaper}>
+                            <Typography className={classes.title}>
+                                Cost to Date: ${totalSpent}
+                            </Typography>
+                        </Paper>
+                    </Grid>
+                    <Grid item>
+                    <Paper className={classes.lastPaper}>
+                            <Typography className={balance < 0 ? classes.balance : classes.title}>
+                                Balance: ${balance}
+                            </Typography>
+                        </Paper>
+                    </Grid>
+
+
         </Grid>
+        </div>
     );
 }
 
